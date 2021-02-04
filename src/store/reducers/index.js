7a9c993d0bf692749f361c92013f017ba82ad2c3
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-02-04 22:27:58 +0500

    Update index.js

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,11 +2,9 @@ import {combineReducers} from 'redux';
 import homeReducer from './homeReducer';
 import becomeDonorReducer from './becomeDonorReducer';
 import findDonorReducer from './findDonorReducer';
-import requestReducer from './requestReducer';
 
 export default combineReducers({
   homeReducer,
   becomeDonorReducer,
   findDonorReducer,
-  requestReducer,
 });
